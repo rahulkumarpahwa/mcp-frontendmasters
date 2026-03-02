e16a433ca405eb3666cf9989931bf99fb34984d1
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2026-03-02 07:06:24 +0530

    till lecture 5

---
+++ mcp.js
@@ -0,0 +1,31 @@
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "add-server",
+  version: "1.0.0",
+});
+
+// Add an addition tool
+server.registerTool(
+  "add",
+  {
+    title: "Addition Tool", // AI decide based upon this to use or not.
+    description: "Add two numbers",
+    inputSchema: { a: z.number(), b: z.number() },
+  },
+  async ({ a, b }) => {
+    return {
+        content: [{ type: "text", text: String(a + b) }],
+    }
+  }
+);
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
